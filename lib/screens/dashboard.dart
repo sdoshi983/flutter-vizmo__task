83177d1c8fed315155ee4f8817d01049077aa828
sdoshi983 <sdoshi983@rku.ac.in>
2022-05-17 23:48:56 +0530

    dashboard page started

---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashbaord'),
+      ),
+    );
+  }
+}
