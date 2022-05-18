e6e017a58bd9d5f6535ff7f8950d0f290bbd461a
sdoshi983 <sdoshi983@rku.ac.in>
2022-05-18 12:13:59 +0530

    profile screen created

---
+++ lib/view/screens/profile/profile.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:vizmo_task/data/constants.dart';
+
+class Profile extends StatefulWidget {
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 4,
+        title: Text('Profile'),
+        backgroundColor: Constants.kThemeColor,
+      ),
+    );
+  }
+}
